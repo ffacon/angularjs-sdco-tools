bfcae7ef4727cc41aee03b9e1a75fd76e0a5462c
a571301 <regis.legrand@worldline.com>
2015-04-21 16:31:42 +0200

    module doc

---
+++ README.md
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+# sdco-tools
+
+## Description
+
+*TODO*
+
+## System Requirements
+
+*TODO*
+
+## Installation
+
+*TODO*
+
+## Running
+
+*TODO*
+
+## API Documentation
+
+*TODO*
+
+## Releasing
+
+*TODO*
+
+## Run Tests
+
+*TODO*
+
+=======
+# angularjs-sdco-tools
+>>>>>>> 04e685342901065220fea2b87649af634211aa3e
---
+++ js/docs-setup.js
@@ -0,0 +1,150 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "sdco-tools",
+      "shortName": "sdco-tools",
+      "type": "overview",
+      "moduleName": "sdco-tools",
+      "shortDescription": "",
+      "keywords": "allows api component components designed evolve generate import libraries library light module multiple overview sdco-tools time transverse"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:jsFiddle",
+      "shortName": "jsFiddle",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Add an html link which allows to open a new jsFiddle",
+      "keywords": "add allows api array blank css data description directive fields framework fwk href html http js jsfiddle link net open sdco-tools string target title values version"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:menuSettings",
+      "shortName": "menuSettings",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Display a basic menu list, and allows to define an action when ",
+      "keywords": "action allows angular api array basic boolean define defining deselected directive display displayed element evaluating execute expression fields formed function label list menu object objects option sdco-tools selected settingscontent"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoCustomEventActions",
+      "shortName": "sdcoCustomEventActions",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "TODO: This directive has to be move to another module.",
+      "keywords": "api directive module move sdco-tools todo"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoEditor",
+      "shortName": "sdcoEditor",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Defines a new editor container, used in conjunction with",
+      "keywords": "add api applied asked behavior button case cases changes compilation compile compileondemand concatenated conjunction container content css default define defines depends directive displayed editor executed height html js jsfiddle link menu option play preview previous processed sdco-tools sdcoeditortab set specific tabs time top true user width zone"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoEditorTab",
+      "shortName": "sdcoEditorTab",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Define the content of a tab for its",
+      "keywords": "api changed color compile compile-on-demand content css define directive document elt escaped false file getelementbyid heading html innertext javascript js js-fiddle main parent red sdco-tools sdcoeditor tab title true type values"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoNotes",
+      "shortName": "sdcoNotes",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Allow to add a note in your views. This note also contain an editable area to let users",
+      "keywords": "add allow api area comments directive editable global note remember save sdco-tools sdconotesexport users view views"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoNotesExport",
+      "shortName": "sdcoNotesExport",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Allow to:",
+      "keywords": "allow api app browser changes data depend directive doesn editable global note notes sdco-tools unique usefull user view"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.directive:sdcoStopKeydownPropagation",
+      "shortName": "sdcoStopKeydownPropagation",
+      "type": "directive",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Stop &#39;keydown&#39; events from the element the directive is placed on",
+      "keywords": "api directive element events keydown sdco-tools"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.service:sdcoEditorService",
+      "shortName": "sdcoEditorService",
+      "type": "service",
+      "moduleName": "sdco-tools",
+      "shortDescription": " ",
+      "keywords": "aggregated aggregation api appears array based behavior content contents css current directive display dom editor editors element field getinstallededitors help indice install installed installeditor instance instances internally js method mode module needed object read-only readonly referencing remove removeeditor retrieve sdco-tools sdo-editor second service shouldn specifies store theelement type typically view xml"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.service:sdcoEditorServiceProvider",
+      "shortName": "sdcoEditorServiceProvider",
+      "type": "service",
+      "moduleName": "sdco-tools",
+      "shortDescription": "Provider of sdcoEditorService",
+      "keywords": "angular api attribute body config content data default editor editors element function isstorageactive module property provider sdco-tools sdcoeditorservice sdcoeditorserviceprovider service stored switching true usefull views yourmpdule"
+    },
+    {
+      "section": "api",
+      "id": "sdco-tools.service:sdcoNotesService",
+      "shortName": "sdcoNotesService",
+      "type": "service",
+      "moduleName": "sdco-tools",
+      "shortDescription": " ",
+      "keywords": "api app array behavior call changes content current data directive directives editor element exportnotes function getglobalnote getnote getviewkey global help import importnotes init install instance internally key listen loadglobalnote loads loadviewnotes local locally localstorage method module needed note notes object parameter position save saved savenote sdco-tools sdonotes sdonotesexport second service shouldn storage store update view"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true,
+      "prettify": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
